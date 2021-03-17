ed365a7b7054f04ad19b0ca2cb8747f04dc679c0
Rafidur <42967404+Rafidur@users.noreply.github.com>
2021-03-17 23:21:03 +0600

    Add files via upload

---
+++ lab task 4/Lab task 4/footer.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<footer class="footer">
+	<hr>
+  	<p style="text-align: center;">Copyright by Md Rafidur Rahman</p>
+</footer>
+</body>
+</html>
